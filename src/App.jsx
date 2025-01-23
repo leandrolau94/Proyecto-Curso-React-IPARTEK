0f88fe0322df55da0fa97813652fd35cf3d379e0
Leandro Daniel Lau Alfonso <l1d9l9a4@gmail.com>
2025-01-23 20:51:44 +0100

    including performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import './App.css'
 import AddTeam from './components/AddTeam';
+import FichaTema from './components/FichaTema';
 
 function App() {
 
-  const [temas, setTemas] = useState(["flores", "dinosaurios"]);
+  const [temas, setTemas] = useState([]);
   const [newTema, setNewTema] = useState("");
 
   const onAddTeam = (e) => {
+    if (newTema.trim().length <= 1) {return};// para que no permita añadir temas vacios ni solamente con 1 letra
     setTemas([...temas, newTema]);
+    setNewTema("");
   };
 
   const onDinamicChange = (e) => {
@@ -19,13 +22,11 @@ function App() {
     <>
       <h1>Galeria de Imagenes</h1>
       <AddTeam onDinamicChange={onDinamicChange} newTema={newTema}/>
-      <ol>
-        {temas.map((tema) => {
-          return (
-            <li key={tema}>{tema}</li>
-          )
-        })}
-      </ol>
+      {temas.map((tema, index) => {
+        return (
+          <FichaTema key={index} titulo={tema} />
+        )
+      })}
       <button onClick={onAddTeam}>Añadir tema</button>
     </>
   )
---
+++ src/components/FichaTema.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FichaTema = ({titulo}) => {
+  return (
+    <h2>{titulo}</h2>
+  )
+}
+
+export default FichaTema
